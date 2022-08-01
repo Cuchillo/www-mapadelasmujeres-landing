490f5eb4c1d61f877df1b1a5cf05ea76c9a8e1dd
Jose Sentis <jose.sentis1987@gmail.com>
2022-08-01 11:24:42 +0200

    WIP: Scroll item abc

---
+++ src/assets/scripts/scroll/ScrollItem__ABC.js
@@ -0,0 +1,41 @@
+import gsap, { Power4 } from 'gsap';
+import { GetBy } from '../_app/cuchillo/core/Element';
+
+import { Scroll } from '../_app/cuchillo/scroll/Scroll';
+import VScroll_Item from '../_app/cuchillo/scroll/VScroll_Item';
+
+class ScrollItem__ABC extends VScroll_Item {
+  _number;
+  _target;
+
+  //==================================================================================================================
+  //          CONSTRUCTOR
+  //==================================================================================================================
+  constructor(__link, __index, __scroller) {
+    super(__link, __index, __scroller);
+
+    this._number = GetBy.selector('.__title b', __link)[0];
+    this._number.innerHTML = '0';
+    this._target = { val: 0 };
+
+    this.opts.offsetShow = window.innerHeight * .75;
+
+    this.onShow = () => {
+      console.log('Show', this.opts.offsetShow);
+
+      gsap.to(this._target, {
+        val: 4.6,
+        duration: 2, 
+        roundProps: 'd',
+        ease: Power4.easeInOut,
+        onUpdate: () => {
+          this._number.innerHTML = `${this._target.val.toFixed(1)}%`.replace('.', ',');
+        }
+      });
+    };
+    this.onHide = () => {};
+    this.onMove = () => {};
+  }
+}
+
+Scroll._registerClass('widget-abc', ScrollItem__ABC);
